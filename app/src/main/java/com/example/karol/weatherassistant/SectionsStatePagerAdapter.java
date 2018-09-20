3bc7d824abc35027a5f7f750de132a683262e317
Karol Kozak <kazmirz30@gmail.com>
2018-09-20 17:04:58 +0200

    Add SectionStatePagerAdapter

---
+++ app/src/main/java/com/example/karol/weatherassistant/SectionsStatePagerAdapter.java
@@ -0,0 +1,34 @@
+package com.example.karol.weatherassistant;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SectionsStatePagerAdapter extends FragmentStatePagerAdapter
+{
+    private final List<Fragment> _fragmentList = new ArrayList<>();
+    private final List<String> _fragmentTitleList = new ArrayList<>();
+
+    public SectionsStatePagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    private void addFragment(Fragment fragment, String title)
+    {
+        _fragmentList.add(fragment);
+        _fragmentTitleList.add(title);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return _fragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return _fragmentList.size();
+    }
+}
