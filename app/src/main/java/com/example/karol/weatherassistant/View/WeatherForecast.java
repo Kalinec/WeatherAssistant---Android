0564503553fb35c52ae18d192b33a1a29b7568c3
Karol Kozak <kazmirz30@gmail.com>
2018-10-15 23:39:51 +0200

    Dalsza implementacja GUI prognozy pogody. Status - nieukończone

--- app/src/main/java/com/example/karol/weatherassistant/View/WeatherForecast.java
+++ app/src/main/java/com/example/karol/weatherassistant/View/WeatherForecast.java
@@ -1,6 +1,8 @@
 package com.example.karol.weatherassistant.View;
 
 
+import android.animation.ObjectAnimator;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,12 +10,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.karol.weatherassistant.R;
 import com.example.karol.weatherassistant.Services.WeatherService;
+import com.github.lzyzsd.circleprogress.ArcProgress;
 
 import org.w3c.dom.Text;
 
@@ -28,10 +32,19 @@ public class WeatherForecast extends Fragment {
     public static TextView Temperature;
     public static TextView Condition;
     public static TextView Description;
-    public static TextView Humidity;
+    //public static TextView Humidity;
+    public static ArcProgress Humidity;
+    public static ArcProgress Cloudiness;
     public static TextView WindSpeed;
+    public static TextView WindDirection;
     public static TextView Pressure;
+    public static TextView Visibility;
     public static TextView Time;
+    public static LinearLayout VisibilityInfo;
+    public static TextView Sunrise;
+    public static TextView Sunset;
+
+
 
     public WeatherForecast() {
         // Required empty public constructor
@@ -51,11 +64,25 @@ public class WeatherForecast extends Fragment {
         Temperature = (TextView) view.findViewById(R.id.textView_temperature);
         Condition = (TextView) view.findViewById(R.id.textView_condition);
         Description = (TextView) view.findViewById(R.id.textView_description);
-        Humidity = (TextView) view.findViewById(R.id.textView_humidityValue);
+        Humidity =  view.findViewById(R.id.arcProgress_humidity);
+        Cloudiness = view.findViewById(R.id.arcProgress_cloudiness);
         WindSpeed = (TextView) view.findViewById(R.id.textView_windSpeedValue);
+        WindDirection = view.findViewById(R.id.textView_windDirectionValue);
         Pressure = (TextView) view.findViewById(R.id.textView_pressureValue);
+        Visibility = view.findViewById(R.id.textView_visibilityValue);
         Time = (TextView) view.findViewById(R.id.textView_updateTimeValue);
+        VisibilityInfo = view.findViewById(R.id.Layout_visibilityInfo);
+        Sunrise = view.findViewById(R.id.textView_sunriseValue);
+        Sunset = view.findViewById(R.id.textView_sunsetValue);
 
+        Humidity.setBottomText(getString(R.string.word_humidity));
+        Humidity.setBottomTextSize(40);
+        Humidity.setFinishedStrokeColor(Color.parseColor("#4A65A6"));
+        Humidity.setUnfinishedStrokeColor(Color.parseColor("#E0E0E0"));
+        Cloudiness.setBottomText(getString(R.string.word_cloudiness));
+        Cloudiness.setBottomTextSize(40);
+        Cloudiness.setFinishedStrokeColor(Color.parseColor("#4A65A6"));
+        Cloudiness.setUnfinishedStrokeColor(Color.parseColor("#E0E0E0"));
 
         //proba pobierania json
        /* try
